d82752e66b0b7edbf3ebcc2c9d8d8629b380b6c7
Thomas Hilberink <thomas.hilberink@outlook.com>
2025-02-28 15:49:45 +0100

    SpriteBatchWrapper

---
+++ frontier/test/src/main/java/com/zhaw/tests/MainTest.java
@@ -0,0 +1,53 @@
+package com.zhaw.tests;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.verification.VerificationMode;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.zhaw.frontier.Main;
+import com.zhaw.frontier.wrappers.SpriteBatchWrapper;
+
+@ExtendWith(GdxExtension.class)
+public class MainTest {
+    private SpriteBatchWrapper spriteBatchWrapperMock;
+    private Main main;
+
+    @BeforeEach
+    void setUp() {
+        spriteBatchWrapperMock = mock(SpriteBatchWrapper.class);
+        main = new Main();
+        main.setSpriteBatchWrapper(spriteBatchWrapperMock);
+        main.create();
+    }
+
+    @Test
+    void testBegin() {
+        main.render();
+        verify(spriteBatchWrapperMock, times(1)).begin();
+    }
+
+    @Test
+    void testEnd() {
+        main.render();
+        verify(spriteBatchWrapperMock, times(1)).end();
+    }
+
+    @Test
+    void testDrawTextureAtPosition() {
+        main.render();
+        verify(spriteBatchWrapperMock, atLeastOnce()).draw(any(Texture.class), eq(140.0f), eq(210.0f));
+    }
+
+
+    @Test
+    void testDispose() {
+        main.dispose();
+        verify(spriteBatchWrapperMock, times(1)).dispose();
+    }
+}
