d8255cd2f40536b5a868b1dfc77c1648482e449e
goncaraf <goncaraf@students.zhaw.ch>
2025-03-19 16:25:13 +0100

    Added test cases for the StartScreen behavior and helper methods

---
+++ frontier/test/src/main/java/com/zhaw/tests/screens/StartScreenTest.java
@@ -0,0 +1,58 @@
+package com.zhaw.tests.screens;
+
+import static org.mockito.Mockito.*;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.zhaw.frontier.FrontierGame;
+import com.zhaw.frontier.screens.StartScreen;
+import com.zhaw.frontier.wrappers.SpriteBatchInterface;
+import com.zhaw.tests.GdxExtension;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith(GdxExtension.class)
+class StartScreenTest {
+
+    private FrontierGame mockGame;
+    private SpriteBatch mockBatch;
+    private StartScreen startScreen;
+    private SpriteBatchInterface mockSpriteBatchWrapper;
+
+    @BeforeEach
+    void setUp() {
+        mockGame = mock(FrontierGame.class);
+        mockBatch = mock(SpriteBatch.class);
+        mockSpriteBatchWrapper = mock(SpriteBatchInterface.class);
+
+        when(mockGame.getBatch()).thenReturn(mockSpriteBatchWrapper);
+        when(mockSpriteBatchWrapper.getBatch()).thenReturn(mockBatch);
+
+        startScreen = spy(new StartScreen(mockGame));
+    }
+
+    @Test
+    void testStartScreenDisposal() {
+        startScreen.hide();
+        verify(startScreen).dispose();
+    }
+
+    @Test
+    void testStartScreenButtonActsProperly() {
+        TextButton startButton = (TextButton) startScreen.getTable().getChildren().first();
+
+        InputEvent event = new InputEvent();
+        event.setType(InputEvent.Type.touchDown);
+        event.setListenerActor(startButton);
+        event.setStage(startScreen.getStage());
+
+        startButton.fire(event);
+        event.setType(InputEvent.Type.touchUp);
+
+        verify(mockGame, never()).switchScreen(any());
+        startButton.fire(event);
+        verify(mockGame).switchScreen(any());
+    }
+}
