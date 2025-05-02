482e91ab2fbb253607c24bc18676fc40c55b61fd
Thomas Hilberink <hilbetho@students.zhaw.ch>
2025-05-02 11:20:35 +0200

    closes 109: added assets for tank and speed enemy

---
+++ frontier/core/src/main/java/com/zhaw/frontier/enums/EnemyType.java
@@ -0,0 +1,52 @@
+package com.zhaw.frontier.enums;
+
+/**
+ * Enum representing different types of enemies in the game.
+ * Each enemy type has a name, health, attack damage, and speed.
+ */
+public enum EnemyType {
+    ORC("orc", 100, 10, 10f),
+    DEMON("demon", 200, 20, 7.5f),
+    GOBLIN("goblin", 70, 15, 12.5f);
+
+    private final String typeName;
+    private final int health;
+    private final float attackDamage;
+    private final float speed;
+
+    /**
+     * Constructor for EnemyType enum.
+     *
+     * @param typeName The name of the enemy type.
+     * @param health The health of the enemy.
+     * @param attackDamage The attack damage of the enemy.
+     * @param speed The speed of the enemy.
+     */
+    EnemyType(String typeName, int health, float attackDamage, float speed) {
+        this.typeName = typeName;
+        this.health = health;
+        this.attackDamage = attackDamage;
+        this.speed = speed;
+    }
+
+    public String getTypeName() {
+        return typeName;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public float getAttackDamage() {
+        return attackDamage;
+    }
+
+    public float getSpeed() {
+        return speed;
+    }
+
+    @Override
+    public String toString() {
+        return typeName;
+    }
+}
